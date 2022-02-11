6f8d90559e3ddd389afa819d1636e076494acc9e
Mariano Rodriguez <marianojrodriguez10@gmail.com>
2022-02-11 17:28:06 -0300

    carga de empleados

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,10 @@
 const table = (employee) => {
   return `
-      <table class="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Fecha</th>
-        </tr>
-      </thead>
-      <tbody>
         <tr>
           <th scope="row">${employee.nombre}</th>
           <td>${employee.apellido}</td>
           <td>${employee.creado}</td>
         </tr>
-      </tbody>
-    </table>
-  
-
     `;
 };
 
--- src/pages/employees/form.js
+++ src/pages/employees/form.js
@@ -1,17 +1,21 @@
+const { createEmployee } = require('../../services/employees.service');
 const formulario = document.getElementById('formulario');
 
 const nombre = document.getElementById('nombre');
 const apellido = document.getElementById('apellido');
-const tipo = document.getElementById('tipo');
 
 formulario.addEventListener('submit', (e) => {
+  //este lo puse para que detenga la recarga cuando haces el submit
   e.preventDefault();
   const nuevoEmpleado = {
     nombre: nombre.value,
     apellido: apellido.value,
-    tipo: tipo.value
   }
-  console.log(nuevoEmpleado);
+  createEmployee(nuevoEmpleado);
+  nombre.value = '';
+  apellido.value = '';
+  console.log('Empleado creado');
+  //console.log(nuevoEmpleado);
   //return nuevoEmpleado;
 });
 
