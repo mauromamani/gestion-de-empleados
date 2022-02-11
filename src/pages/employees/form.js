6401e1adb37736416738cb50f6b82dc00b8df0c0
Mariano Rodriguez <marianojrodriguez10@gmail.com>
2022-02-11 19:27:19 -0300

    agregando validaciones html

--- src/pages/employees/form.js
+++ src/pages/employees/form.js
@@ -1,23 +1,43 @@
 const { createEmployee } = require('../../services/employees.service');
 const formulario = document.getElementById('formulario');
-
 const nombre = document.getElementById('nombre');
 const apellido = document.getElementById('apellido');
 
-formulario.addEventListener('submit', (e) => {
+
+
+formulario.addEventListener('submit', async (e) => {
   //este lo puse para que detenga la recarga cuando haces el submit
   e.preventDefault();
+
+  /*if (!nombre.value.trim()) {
+    alerta.textContent = 'campos obligatorios';
+    alerta.style.display = 'block';
+    return;
+  }*/
+
   const nuevoEmpleado = {
     nombre: nombre.value,
     apellido: apellido.value,
   }
-  createEmployee(nuevoEmpleado);
-  nombre.value = '';
-  apellido.value = '';
-  console.log('Empleado creado');
+  try {
+    await createEmployee(nuevoEmpleado);
+    nombre.value = '';
+    apellido.value = '';
+    console.log('Empleado creado');
+  }
+  catch (err) {
+    //mostrar alerta de error de carga
+    console.log('error');
+  }
+
   //console.log(nuevoEmpleado);
   //return nuevoEmpleado;
-});
+})
+
+
+
+
+
 
 /*const enviarEmpleado = (nuevoEmpleado) => {
   console.log(nuevoEmpleado);
