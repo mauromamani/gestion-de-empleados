f24947bcbd5cd46f58b69c91738bb6a05c073688
mauromamani <mauromamani.b@gmail.com>
2022-02-11 22:32:39 -0300

    integrar nav, eliminar employees.html,js

--- src/pages/employees/form.js
+++ src/pages/employees/form.js
@@ -1,11 +1,15 @@
 const { createEmployee } = require('../../services/employees.service');
+
+// Html ref's
 const formulario = document.getElementById('formulario');
 const nombre = document.getElementById('nombre');
 const apellido = document.getElementById('apellido');
 
+// Events
+formulario.addEventListener('submit', formHandler);
 
-
-formulario.addEventListener('submit', async (e) => {
+// Events functions
+async function formHandler(e) {
   //este lo puse para que detenga la recarga cuando haces el submit
   e.preventDefault();
 
@@ -18,26 +22,20 @@ formulario.addEventListener('submit', async (e) => {
   const nuevoEmpleado = {
     nombre: nombre.value,
     apellido: apellido.value,
-  }
+  };
   try {
     await createEmployee(nuevoEmpleado);
     nombre.value = '';
     apellido.value = '';
     console.log('Empleado creado');
-  }
-  catch (err) {
+  } catch (err) {
     //mostrar alerta de error de carga
     console.log('error');
   }
 
   //console.log(nuevoEmpleado);
   //return nuevoEmpleado;
-})
-
-
-
-
-
+}
 
 /*const enviarEmpleado = (nuevoEmpleado) => {
   console.log(nuevoEmpleado);
---
+++ src/pages/home/home.js
@@ -0,0 +1,28 @@
+const { getAllEmployees } = require('../../services/employees.service');
+const { table } = require('../../components/Table');
+// HTMl ref's
+const employeesTable = document.getElementById('employees-table');
+
+// Events
+window.addEventListener('DOMContentLoaded', loadEmployees);
+
+// Functions
+
+/**
+ * @name loadEmployees
+ * @description agrega los empleados al documento
+ */
+async function loadEmployees() {
+  const employees = await getAllEmployees();
+
+  if (!employees.length) {
+    employeesTable.innerHTML = `
+      <h1>No hay empleados cargados</h1>
+    `;
+    return;
+  }
+
+  employees.forEach((employee) => {
+    employeesTable.innerHTML += table(employee);
+  });
+}
