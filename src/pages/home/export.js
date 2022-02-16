7adc07b1489ce7222b0451d064cc583900255881
Mariano Rodriguez <marianojrodriguez10@gmail.com>
2022-02-16 17:41:35 -0300

    exportar tabla terminado

--- src/pages/home/export.js
+++ src/pages/home/export.js
@@ -1,4 +1,7 @@
 const Swal = require('sweetalert2');
+const { formatDate } = require('../../utils/formatDate');
+const { getAllEmployees } = require('../../services/employees.service');
+const { exportar } = require('../../utils/export-table');
 // Definicion de las funciones de los botones de la tabla
 const exportTable = async () => {
   const result = await Swal.fire({
@@ -14,6 +17,18 @@ const exportTable = async () => {
   if (result.isConfirmed) {
     try {
       //la funcion va acá
+
+      const employees = await getAllEmployees();
+
+      // creo un nuevo arreglo de objetos, ya que es necesario que las columnas coincidan
+      //acá llamo a la funcion para formatear la fecha y mandarla directamente a la vista ya formateada
+      const ar = employees.map((empleado) => ({
+        nombre: empleado.nombre,
+        apellido: empleado.apellido,
+        creado: formatDate(empleado.creado),
+      }));
+      //exportamos la tabla a un archivo csv
+      exportar(ar);
       await Swal.fire(
         'Realizado',
         `La Tabla fue exportada con éxito`,
