9c60f434445a6b6ea30d364dca7a3353dcd3ac4c
Mariano Rodriguez <marianojrodriguez10@gmail.com>
2022-03-15 16:49:19 -0300

    modificando navbar

--- src/pages/employees/form-create.js
+++ src/pages/employees/form-create.js
@@ -1,16 +1,22 @@
 const { createEmployee } = require('../../services/employees.service');
 const { alertSuccess, alertWarning } = require('../../utils/swal');
+const { navbar } = require('../../components/navbar');
 
 // Html ref's
 const formulario = document.getElementById('formulario');
 const nombre = document.getElementById('nombre');
 const apellido = document.getElementById('apellido');
 const tipo = document.getElementById('tipo');
+const nav = document.getElementById('navbar');
 
 // Events
 formulario.addEventListener('submit', formHandler);
+window.addEventListener('DOMContentLoaded', DOMLoadedHandler);
 
 // Events functions
+function DOMLoadedHandler() {
+  nav.innerHTML = navbar(false);
+}
 async function formHandler(e) {
   e.preventDefault();
 
