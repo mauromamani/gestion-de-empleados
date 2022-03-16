fe967b8bf7cfc51f5584668245ac7be505ab02f2
Mariano Rodriguez <marianojrodriguez10@gmail.com>
2022-03-16 19:54:20 -0300

    redireccionamiento a modificar

--- src/pages/employees/employee.js
+++ src/pages/employees/employee.js
@@ -2,16 +2,18 @@ const { navbar } = require('../../components/navbar');
 const { getEmployeeById } = require('../../services/employees.service');
 
 const nav = document.getElementById('navbar');
-
+const boton = document.getElementById('modificar');
 window.addEventListener('DOMContentLoaded', DOMLoadedHandler);
-
 const employeeId = localStorage.getItem('employee-id');
 
 async function DOMLoadedHandler() {
   nav.innerHTML = navbar(false);
-
   const employee = await getEmployeeById(parseInt(employeeId));
   console.log(employee);
-
+  boton.onclick = modificar;
+  function modificar() {
+    localStorage.setItem('id', employee.id);
+    window.location.href = '../employees/form-edit.html';
+  }
   // Acá se cargaría la información a mostrar en el HTML
 }
