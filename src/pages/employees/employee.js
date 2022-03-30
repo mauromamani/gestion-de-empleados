21bd94d8d8ab6ca6d7c323a47ad4d0d8192a7a1c
Mariano Rodriguez <marianojrodriguez10@gmail.com>
2022-03-30 18:45:18 -0300

    agregado de eliminar empleado

--- src/pages/employees/employee.js
+++ src/pages/employees/employee.js
@@ -1,7 +1,13 @@
 const { navbar } = require('../../components/navbar');
 const { getEmployeeById } = require('../../services/employees.service');
 const { formatDateNac, formatDate } = require('../../utils/formatDate');
-
+const { getCurrentUser } = require('../../utils/getCurrentUser');
+const {
+  alertConfirmation,
+  alertSuccess,
+  alertWarning,
+} = require('../../utils/swal');
+const { deleteEmployee } = require('../../services/employees.service');
 const nombre = document.getElementById('nombre');
 const apellido = document.getElementById('apellido');
 const dni = document.getElementById('dni');
@@ -16,20 +22,22 @@ const creado = document.getElementById('creado');
 const estado = document.getElementById('estado');
 const fechaEstado = document.getElementById('fechaEstado');
 const nav = document.getElementById('navbar');
-const boton = document.getElementById('modificar');
+const botonModificar = document.getElementById('modificar');
 const fotoPerfil = document.getElementById('fotoPerfil');
 const fotoDni1 = document.getElementById('fotoDni1');
 const fotoDni2 = document.getElementById('fotoDni2');
+const botonEliminar = document.getElementById('eliminar');
+const divEliminar = document.getElementById('divEliminar');
 window.addEventListener('DOMContentLoaded', DOMLoadedHandler);
 const employeeId = localStorage.getItem('employee-id');
-
 async function DOMLoadedHandler() {
+  const currentUser = getCurrentUser();
   nav.innerHTML = navbar(false);
   const employee = await getEmployeeById(parseInt(employeeId));
   // console.log(employee);
   //cuando se aprieta en modificar se manda el id del empleado al formulario
   //para modificarlo
-  boton.onclick = modificar;
+  botonModificar.onclick = modificar;
   function modificar() {
     localStorage.setItem('id', employee.id);
     window.location.href = '../employees/form-edit.html';
@@ -50,27 +58,48 @@ async function DOMLoadedHandler() {
     employee.estado === 'ALTA'
       ? `<h5 style="color: #00bc8c;">${employee.estado}</h5>`
       : employee.estado === 'PROCESO'
-      ? `<h5 style="color: #f39c12;">${employee.estado}</h5>`
-      : employee.estado === 'BAJA'
-      ? `<h5 style="color: #e74c3c;">${employee.estado}</h5>`
-      : '';
+        ? `<h5 style="color: #f39c12;">${employee.estado}</h5>`
+        : employee.estado === 'BAJA'
+          ? `<h5 style="color: #e74c3c;">${employee.estado}</h5>`
+          : '';
   //falta formatear fecha estado
   fechaEstado.innerHTML = employee.fechaAlta ? employee.fechaAlta : '-';
 
   // fotoPerfil.innerHTML = employee.imgPerfil.toString('base64');
   fotoPerfil.innerHTML = employee.imgPerfil
     ? `<img src="data:image/jpeg;base64,${employee.imgPerfil.toString(
-        'base64'
-      )}" class="img-fluid rounded" style="width: 350px; height: 200px" /> `
+      'base64'
+    )}" class="img-fluid rounded" style="width: 350px; height: 200px" /> `
     : null;
   fotoDni1.innerHTML = employee.imgDniFrontal
     ? `<img src="data:image/jpeg;base64,${employee.imgDniFrontal.toString(
-        'base64'
-      )}"  style="width: 350px; height: 200px;" /> `
+      'base64'
+    )}"  style="width: 350px; height: 200px;" /> `
     : null;
   fotoDni2.innerHTML = employee.imgDniTrasera
     ? `<img src="data:image/jpeg;base64,${employee.imgDniTrasera.toString(
-        'base64'
-      )}"  style="width: 350px ; height: 200px;" /> `
+      'base64'
+    )}"  style="width: 350px ; height: 200px;" /> `
     : null;
+  if (currentUser.rol === 'ADMIN') {
+    divEliminar.style.display = "block";
+  }
+  botonEliminar.onclick = eliminar;
+  async function eliminar(e) {
+    const result = await alertConfirmation('Seguro de que quiere eliminar el empleado?', null);
+    if (result.isConfirmed) {
+      try {
+        await deleteEmployee(employee.id);
+        alertSuccess(`El empleado fue eliminado con exito`);
+
+      }
+      catch (error) {
+        console.log(error);
+        alertWarning();
+      }
+
+    }
+    e.preventDefault();
+    window.location.href = '../home/home.html';
+  }
 }
